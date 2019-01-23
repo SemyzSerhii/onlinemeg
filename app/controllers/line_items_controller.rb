25662a5624b5c0e413d051c08770ecff1ce71e02
Semyz Serhii <semizserhii@gmail.com>
2019-01-24 00:11:58 +0200

    fix style product and js quantity for navbar cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -4,11 +4,8 @@ class LineItemsController < ApplicationController
     @product = Product.find(params[:product_id])
     @line_item = @cart.add_product(@product)
 
-    respond_to do |format|
       if @line_item.save
-        format.html {redirect_to @line_item.cart, notice: 'Товар добавлено' }
-        format.json { render action: 'show', status: :created, location: @line_item }
+        respond_to :js
       end
-    end
   end
 end
