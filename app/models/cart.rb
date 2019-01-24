caee51e862330bb01159eeecfd66ce58dfddb092
Semyz Serhii <semizserhii@gmail.com>
2019-01-24 19:47:40 +0200

    fix current_cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,7 +3,7 @@ class Cart < ApplicationRecord
 
   def add_product(product)
     current_item = line_items.find_by(product: product)
-    if current_item
+    if current_item.present?
       current_item.quantity += 1
     else
       current_item = line_items.build(product: product)
